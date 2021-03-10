72cf97b8c9909496b1a0ad5ac37c863a4ad3178d
uuljan <u.aitnazarova@gmail.com>
2021-03-10 16:15:46 +0600

    Created Issue, Status, Type models, made migrations

--- source/tracker/models.py
+++ source/tracker/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Issue(models.Model):
+    summary = models.CharField(max_length=200, blank=False, null=False)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    status = models.ForeignKey('tracker.Status', on_delete=models.PROTECT)
+    type = models.ForeignKey('tracker.Type', on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Status(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+
+class Type(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
