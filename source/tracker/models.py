0892c6f72e1771be1d1e35350331745940255e00
uuljan <u.aitnazarova@gmail.com>
2021-03-10 16:25:16 +0600

    Updated models by adding magic method __str__

--- source/tracker/models.py
+++ source/tracker/models.py
@@ -8,8 +8,20 @@ class Issue(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.summary
+
+
 class Status(models.Model):
     name = models.CharField(max_length=20, blank=False, null=False)
 
+    def __str__(self):
+        return self.name
+
 class Type(models.Model):
     name = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+    
