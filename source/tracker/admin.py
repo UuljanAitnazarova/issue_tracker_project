0d00d26e76c0a69039b0c6a72947554f37a84e88
uuljan <u.aitnazarova@gmail.com>
2021-03-10 16:21:35 +0600

    Created superuser, registered models in amdin panel

--- source/tracker/admin.py
+++ source/tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from tracker.models import Issue, Status, Type
+
+admin.site.register(Issue)
+admin.site.register(Status)
+admin.site.register(Type)
