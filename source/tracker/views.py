bf92d98c3962907f58867f285ff430d44361ca47
uuljan <u.aitnazarova@gmail.com>
2021-03-11 14:59:29 +0600

    Updated templates with some bootstrap

--- source/tracker/views.py
+++ source/tracker/views.py
@@ -8,6 +8,7 @@ class IndexView(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
+        print(kwargs)
         context = super().get_context_data(**kwargs)
         context['issues'] = Issue.objects.all()
         return context
@@ -16,8 +17,11 @@ class IssueDetailView(TemplateView):
     template_name = 'issue_detail.html'
 
     def get_context_data(self, **kwargs):
+        print(kwargs)
         context = super().get_context_data(**kwargs)
+
         context['issue'] = get_object_or_404(Issue, pk=kwargs['issue_pk'])
+        print(context)
         return context
 
 
